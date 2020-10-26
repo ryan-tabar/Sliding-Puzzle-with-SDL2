8ccba924a15199bccbcc23a1ac34c0729b379a39
SirFourier <ryan.tabar321@gmail.com>
2020-10-26 19:08:48 +0000

    window and basic tile render done

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,105 @@
 //Using SDL and standard IO
-#include <SDL.h>
-#include <stdio.h>
-#include "print.h"
+#include<SDL.h>
+#include<SDL_ttf.h>
+#include<iostream>
+#include "tile.h"
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+int main( int argc, char* args[] ) {
+    // Screen dimensions
+    const int SCREEN_WIDTH = 600;
+    const int SCREEN_HEIGHT = 600;
 
-int main( int argc, char* args[] )
-{
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    } else {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL ) {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        } else {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0, 255, 0 ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Wait two seconds
-            SDL_Delay( 2000 );
-        }
+    // Initialise SDL video subsystem
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL could not initialise! Error: " << SDL_GetError() << std::endl;
+        return -1;
     }
-}
+
+    // Intialise SDL_ttf
+    if (TTF_Init() == -1) {
+        std::cout << "SDL_ttf could not initialise! Error: " << TTF_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("Checkers", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cout << "SDL could not create window! Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if (renderer == nullptr) {
+        std::cout << "SDL could not create renderer! Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Load font for text
+    const int fontSize = 30;
+    TTF_Font* font = TTF_OpenFont("assets/octin sports free.ttf", fontSize);
+    if (font == nullptr) {
+        std::cout << "Failed to load font! Error: " << TTF_GetError() << std::endl;
+        return -1;
+    }
+
+    // Testing
+    SDL_Rect rect = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, 50};
+    SDL_Color colour = {255, 123, 43, 255};
+    Tile tile(&rect, &colour, "10", font, renderer);
+
+    // Game loop variables
+    bool stop = false;
+    SDL_Event event;
+
+    // Game loop
+    while (!stop) {
+        // Handle events
+        while (SDL_PollEvent(&event) != 0) {
+            // Handle quiting
+            if (event.type == SDL_QUIT) {
+                stop = true;
+            }
+        }
+
+        // update
+        tile.move(1, 1);
+
+        // render
+        // for each tile, 
+            // if it needs to be re-rendered 
+                // render it
+
+        // Test render
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        tile.render(renderer);
+
+        // Update screen from backbuffer and clear backbuffer
+        SDL_RenderPresent(renderer);
+        
+        // Slow down program
+        SDL_Delay(20);
+
+    }
+
+    // Free textures
+    tile.free();
+
+    // Destroy renderer and window
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    renderer = nullptr;
+    window = nullptr;
+
+    // Free font
+    TTF_CloseFont(font);
+    font = nullptr;
+
+    // Quit SDL
+    SDL_Quit();
+
+    // Exit program
+    std::cout << "Exiting program..." << std::endl;
+    return 0;
+}   
