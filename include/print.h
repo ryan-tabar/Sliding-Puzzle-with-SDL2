6369f708920c243a078fbb3476eecd6e7a52855c
SirFourier <ryan.tabar321@gmail.com>
2020-10-26 02:47:50 +0000

    first commit

---
+++ include/print.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<iostream>
+
+void print(const char* string);
+void print2(const char* string);
---
+++ src/print.cpp
@@ -0,0 +1,9 @@
+#include "print.h"
+
+void print(const char* string) {
+    std::cout << string << std::endl;
+}
+
+void print2(const char* string) {
+    std::cout << string << " 2" << std::endl;
+}
