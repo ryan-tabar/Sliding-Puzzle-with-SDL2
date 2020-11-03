45798fee594385c627ba2f183649507c0c2979f9
SirFourier <ryan.tabar321@gmail.com>
2020-11-03 04:15:33 +0000

    added comment

--- include/button.h
+++ include/button.h
@@ -3,11 +3,12 @@
 #include <SDL_ttf.h>
 #include "userInterface.h"
 
+// Currently empty class but still used just incase button classs requires additional functionalities
 class Button : public UserInterface {
     private:
 
     public:
         Button(const SDL_Rect& rect, const SDL_Color& colour, TTF_Font* const font, const SDL_Color& fontColour);
-
+        
 
 };
